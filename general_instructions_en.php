641340f21d8bbeda44b742d0262b37a6d40a93da
erahulkulkarni <erahulkulkarni@gmail.com>
2025-12-22 13:58:33 +0530

    Improve clarity of program indentation instructions
    Clarified instruction about code formatting

--- general_instructions_en.php
+++ general_instructions_en.php
@@ -111,7 +111,7 @@
 					Four spaces, by use of spacebar key of keyboard
 			
 			Not a part of syntax, but
-				please do not entire program beginning on same column
+				please do not code entire program beginning on same column, entire code should not begin on same column
 		*/
 	?>
 	
